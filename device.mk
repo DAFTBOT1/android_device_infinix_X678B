ed9233056f656a282c8c145dc012af2c7297103e
LuK1337 <priv.luk@gmail.com>
2026-03-08 09:39:19 +0800

    X678B: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -315,6 +315,9 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayVeritas \
     SystemUIResOverlayVeritas
 
+PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
+
 # Lineage-Specific Overlays
 PRODUCT_PACKAGES += \
     ApertureOverlay
